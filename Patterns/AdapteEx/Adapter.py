da4c9e83a3e9d6e5c17d4030a23f57dcbcfc62b5
Anton Baksheiev <abaksheiev@gmail.com>
2023-12-09 14:09:25 +0100

    add adaptee/decorator

---
+++ Patterns/AdapteEx/Adapter.py
@@ -0,0 +1,11 @@
+from Adaptee import Adaptee
+
+
+class Adapter(Adaptee):
+    """
+    The Adapter makes the Adaptee's interface compatible with the Target's
+    interface via multiple inheritance.
+    """
+
+    def request(self) -> str:
+        return f"Adapter: (TRANSLATED) {self.specific_request()[::-1]}"
